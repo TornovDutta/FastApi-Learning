65ab1cc08c554e85b82a07c79d67a1fb084bfeb9
TornovDutta <tornovdutta@gmail.com>
2025-12-03 12:41:48 +0530

    add the home and param path

--- introduction/main.py
+++ introduction/main.py
@@ -4,3 +4,18 @@ app=FastAPI()
 @app.get("/")
 def root():
     return "hello world"
+
+@app.get("/home")
+def home():
+    return {"home":{
+        "dashboard",
+        "homepage"
+    },
+    "hello":{
+        "hello world",
+        "hey!!"
+    }}
+
+@app.get("/param/{id}")
+def param(id):
+    return int(id) 
