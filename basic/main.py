3814473c5fd94206f1b7b4660e4c45f364b14f0c
TornovDutta <tornovdutta@gmail.com>
2025-12-05 13:28:58 +0530

    add the query router and requested body and post router and dynamic router

--- basic/main.py
+++ basic/main.py
@@ -1,2 +1,36 @@
 from fastapi import FastAPI
-app 
+from typing import Optional
+from  pydantic import BaseModel
+app = FastAPI()
+class Blogs(BaseModel):
+    title:str
+    published:bool
+    date:Optional[str]
+
+@app.get("/")
+def index():
+    return {"data": "this is blogs page"}
+
+@app.get("/about")
+def about():
+    return {"data": "this is about page"}
+
+@app.get("/blogs/published")
+def published():
+    return {"data": "page for published blogs"}
+
+@app.get("/blogs/{id}")
+def blogs(id: int):
+    return {"data": f"{id} blogs are there"}
+
+@app.get("/blogs")
+def query(limit=10,published:bool=True, sort:Optional[bool]=None):
+    if published:
+        return {"data":f"{limit} of published bolgs"}
+    else:
+        return {"data":f"{limit} of bolgs"}
+    
+@app.post("/blogs")
+def postRounter(blogs:Blogs):
+    return blogs
+
